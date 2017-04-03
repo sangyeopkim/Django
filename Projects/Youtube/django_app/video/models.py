3c9bdc3ea0fd024bacad0efc9d9c33c35ee5964c
sangyeopkim <yeop123456@gmail.com>
2017-04-03 13:10:48 +0900

    Video class 생성 및 migrate

--- Projects/Youtube/django_app/video/models.py
+++ Projects/Youtube/django_app/video/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Video(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(blank=True)
-    youtube_id = models.CharField(max_length=100)
+    youtube_id = models.CharField(unique=True, max_length=100)
     published_date = models.DateTimeField()
 
     def __str__(self):
